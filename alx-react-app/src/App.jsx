fbd70b9ccd3bfd8cbd780d0091111cd87ad9677d
Zola <126443383+zolakulani@users.noreply.github.com>
2025-07-11 09:42:04 +0200

    feat: implement UserProfile component with name, age, and bio props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 import Footer from './components/Footer'
 import './App.css'
 
@@ -35,6 +36,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   )
