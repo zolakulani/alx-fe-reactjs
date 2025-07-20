8cd87d0c0c969743a08841dee916997beafa340e
Zola <126443383+zolakulani@users.noreply.github.com>
2025-07-20 17:19:25 +0200

    feat: add Counter component with increment/decrement/reset functionality

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent'
 import UserProfile from './components/UserProfile'
 import Footer from './components/Footer'
+import Counter from './components/Counter';
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
+      <Counter/>
     </>
   )
 }
