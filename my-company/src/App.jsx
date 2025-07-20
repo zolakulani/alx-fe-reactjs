27ccb17e576f61dd247ae50dc219b1f5e3a2342d
Zola <126443383+zolakulani@users.noreply.github.com>
2025-07-20 22:14:26 +0200

    Fix: move Contact.jsx to components and update App.jsx import

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import Contact from './pages/Contact';
+import Contact from './components/Contact';
 
 function App() {
   return (
