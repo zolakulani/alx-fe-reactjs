781440d3a81a6b3e8831de09a68ce779a82b9a47
Zola <126443383+zolakulani@users.noreply.github.com>
2025-07-20 15:28:28 +0200

    Add Styling, States, Rendering, Routes, and Hooks project (fixed MainContent styling)

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 <span style={{ fontWeight: 'bold'}}>City</span> <span style={{ fontWeight: 'bold', color: 'red' }}>Lovers</span></p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: "20px", backgroundColor: "#f5f5f5", borderRadius: "8px" }}>
+      <p style={{ fontStyle: "italic", color: "#333" }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
